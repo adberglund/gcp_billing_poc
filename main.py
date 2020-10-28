cc244c5beede41bae52f29852593a5cd9fda0e37
Andrew Berglund <aberglun@redhat.com>
2020-10-28 10:08:56 -0400

    Add print messages + last modified func

--- main.py
+++ main.py
@@ -19,24 +19,37 @@ def check_access(project):
         "permissions": [
             "bigquery.jobs.create",
             "bigquery.tables.getData",
-            "bigquery.tables.list"
+            "bigquery.tables.list",
+            "bigquery.tables.get"
         ]
     }
 
     request = service.projects().testIamPermissions(resource=project, body=check_permissions)
     response = request.execute()
-
+    print(f"Checking for permissions in project {project}")
     # We would want to check the response to make sure our required permissions are in the response
     print(response)
 
 
 def get_table_id(data_set):
     """Get the billing table from a dataset in the format projectID.dataset"""
+    print(f"Getting table ID for dataset {data_set}")
     client = bigquery.Client()
     for table in client.list_tables(data_set):
         if "gcp_billing_export" in table.full_table_id:
-            return table.full_table_id
+            table_id = table.full_table_id.replace(":", ".")
+            print(f"Table ID: {table_id}")
+            return table_id
     return None
+
+
+def get_last_modified(table_id):
+    """Get the last modified date for the table."""
+    client = bigquery.Client()
+    billing_table = client.get_table(table_id)
+    last_modified = billing_table.modified
+    print(f"Table last modified: {last_modified}")
+    return last_modified
 
 
 def query_table(table_id):
@@ -83,10 +96,12 @@ def query_table(table_id):
         writer = csv.writer(f)
         for row in query_job:
             writer.writerow(row)
+    print("Wrote query results to file.")
 
 
 if __name__ == "__main__":
-    check_access("")
-    table_id = get_table_id("")
+    check_access("")  # Add project ID
+    table_id = get_table_id("")  # Add projectID.dataset
+    get_last_modified(table_id)
     query_table(table_id)
 
